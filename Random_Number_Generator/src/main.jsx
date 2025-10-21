2c946a4a584956178941633095499b87049339f8
NuthaN-444 <nuthanprasadkg@gmail.com>
2025-10-21 11:35:47 +0530

    uploaded

--- Random_Number_Generator/src/main.jsx
+++ Random_Number_Generator/src/main.jsx
@@ -1,17 +1,10 @@
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Reactjs_Things/Random_Number_Generator">
-    <Routes>
-          <Route path='/' element={<App />}/>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
